157b0f05c63e143b20d4b98072c09d44713c3d66
Andriy Redko <andriy.redko@aiven.io>
2022-02-08 15:57:03 -0500

    Fixing JDK-11 and HighlighterSearchIT::testPostingsHighlighter
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- server/src/internalClusterTest/java/org/opensearch/search/fetch/subphase/highlight/HighlighterSearchIT.java
+++ server/src/internalClusterTest/java/org/opensearch/search/fetch/subphase/highlight/HighlighterSearchIT.java
@@ -2282,7 +2282,6 @@ public class HighlighterSearchIT extends OpenSearchIntegTestCase {
         assertHighlight(response, 0, "text", 1, 2, equalTo("This is the <em>fifth</em> sentence"));
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/2063")
     public void testPostingsHighlighter() throws Exception {
         assertAcked(prepareCreate("test").addMapping("type1", type1PostingsffsetsMapping()));
         ensureGreen();
@@ -3288,7 +3287,6 @@ public class HighlighterSearchIT extends OpenSearchIntegTestCase {
         );
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/2063")
     public void testCopyToFields() throws Exception {
         XContentBuilder b = jsonBuilder().startObject().startObject("properties");
         b.startObject("foo");
@@ -3297,7 +3295,8 @@ public class HighlighterSearchIT extends OpenSearchIntegTestCase {
             b.field("copy_to", "foo_copy");
         }
         b.endObject();
-        b.startObject("foo_copy").field("type", "text").endObject();
+        // If field is not stored, it is looked up in source (but source has only 'foo'
+        b.startObject("foo_copy").field("type", "text").field("store", true).endObject();
         b.endObject().endObject();
         prepareCreate("test").addMapping("type", b).get();
 
@@ -3411,7 +3410,6 @@ public class HighlighterSearchIT extends OpenSearchIntegTestCase {
         assertThat(field.getFragments()[0].string(), equalTo("<em>brown</em>"));
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/2063")
     public void testHighlightQueryRewriteDatesWithNow() throws Exception {
         assertAcked(
             client().admin()
@@ -3448,7 +3446,6 @@ public class HighlighterSearchIT extends OpenSearchIntegTestCase {
         }
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/2063")
     public void testWithNestedQuery() throws Exception {
         String mapping = Strings.toString(
             jsonBuilder().startObject()
