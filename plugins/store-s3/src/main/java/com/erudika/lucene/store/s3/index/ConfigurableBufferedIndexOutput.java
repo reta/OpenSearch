ba355825dbd49dd772394d0eec91eefa50c7aa15
Andriy Redko <andriy.redko@aiven.io>
2024-02-02 15:09:25 -0500

    Initial implementation (no shards)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

---
+++ plugins/store-s3/src/main/java/com/erudika/lucene/store/s3/index/ConfigurableBufferedIndexOutput.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2004-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.erudika.lucene.store.s3.index;
+
+import org.apache.lucene.store.IndexOutput;
+
+import java.io.IOException;
+
+/**
+ * A simple base class that performs index output memory based buffering. The buffer size if configurable.
+ *
+ * @author kimchy
+ */
+// NEED TO BE MONITORED AGAINST LUCENE
+public abstract class ConfigurableBufferedIndexOutput extends IndexOutput {
+
+    public static final int DEFAULT_BUFFER_SIZE = 16384;
+
+    private byte[] buffer;
+    private long bufferStart = 0; // position in file of buffer
+    private int bufferPosition = 0; // position in buffer
+
+    protected int bufferSize = DEFAULT_BUFFER_SIZE;
+
+    protected ConfigurableBufferedIndexOutput(final String resourceDescription) {
+        super(resourceDescription, "ConfigurableBufferedIndexOutput");
+    }
+
+    protected void initBuffer(final int bufferSize) {
+        this.bufferSize = bufferSize;
+        buffer = new byte[bufferSize];
+    }
+
+    /**
+     * Writes a single byte.
+     *
+     * @see org.apache.lucene.store.IndexInput#readByte()
+     */
+    @Override
+    public void writeByte(final byte b) throws IOException {
+        if (bufferPosition >= bufferSize) {
+            flush();
+        }
+        buffer[bufferPosition++] = b;
+    }
+
+    /**
+     * Writes an array of bytes.
+     *
+     * @param b the bytes to write
+     * @param length the number of bytes to write
+     * @see org.apache.lucene.store.IndexInput#readBytes(byte[],int,int)
+     */
+    @Override
+    public void writeBytes(final byte[] b, final int offset, final int length) throws IOException {
+        int bytesLeft = bufferSize - bufferPosition;
+        // is there enough space in the buffer?
+        if (bytesLeft >= length) {
+            // we add the data to the end of the buffer
+            System.arraycopy(b, offset, buffer, bufferPosition, length);
+            bufferPosition += length;
+            // if the buffer is full, flush it
+            if (bufferSize - bufferPosition == 0) {
+                flush();
+            }
+        } else {
+            // is data larger then buffer?
+            if (length > bufferSize) {
+                // we flush the buffer
+                if (bufferPosition > 0) {
+                    flush();
+                }
+                // and write data at once
+                flushBuffer(b, offset, length);
+                bufferStart += length;
+            } else {
+                // we fill/flush the buffer (until the input is written)
+                int pos = 0; // position in the input data
+                int pieceLength;
+                while (pos < length) {
+                    pieceLength = length - pos < bytesLeft ? length - pos : bytesLeft;
+                    System.arraycopy(b, pos + offset, buffer, bufferPosition, pieceLength);
+                    pos += pieceLength;
+                    bufferPosition += pieceLength;
+                    // if the buffer is full, flush it
+                    bytesLeft = bufferSize - bufferPosition;
+                    if (bytesLeft == 0) {
+                        flush();
+                        bytesLeft = bufferSize;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Forces any buffered output to be written.
+     */
+    public void flush() throws IOException {
+        flushBuffer(buffer, bufferPosition);
+        bufferStart += bufferPosition;
+        bufferPosition = 0;
+    }
+
+    /**
+     * Expert: implements buffer write. Writes bytes at the current position in the output.
+     *
+     * @param b the bytes to write
+     * @param len the number of bytes to write
+     */
+    private void flushBuffer(final byte[] b, final int len) throws IOException {
+        flushBuffer(b, 0, len);
+    }
+
+    /**
+     * Expert: implements buffer write. Writes bytes at the current position in the output.
+     *
+     * @param b the bytes to write
+     * @param offset the offset in the byte array
+     * @param len the number of bytes to write
+     */
+    protected abstract void flushBuffer(byte[] b, int offset, int len) throws IOException;
+
+    /**
+     * Closes this stream to further operations.
+     */
+    @Override
+    public void close() throws IOException {
+        flush();
+    }
+
+    /**
+     * Returns the current position in this file, where the next write will occur.
+     *
+     * @see #seek(long)
+     */
+    @Override
+    public long getFilePointer() {
+        return bufferStart + bufferPosition;
+    }
+
+    /**
+     * Sets current position in this file, where the next write will occur.
+     *
+     * @see #getFilePointer()
+     */
+    public void seek(final long pos) throws IOException {
+        flush();
+        bufferStart = pos;
+    }
+
+    /**
+     * The number of bytes in the file.
+     */
+    public abstract long length() throws IOException;
+
+}
---
+++ plugins/store-s3/src/main/java/org/opensearch/plugin/store/s3/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/**
+ * S3 store plugin
+ */
+package org.opensearch.plugin.store.s3;
